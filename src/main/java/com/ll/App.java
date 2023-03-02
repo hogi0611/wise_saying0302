4696389c6cc7d1fcb3df2fd0d56a500be8f1f323
hogi0611 <hogi_@naver.com>
2023-03-02 13:11:30 +0900

    1단계, 구현완료

---
+++ src/main/java/com/ll/App.java
@@ -0,0 +1,25 @@
+package com.ll;
+
+import java.util.Scanner;
+
+public class App {
+    private final Scanner sc;
+
+    public  App(Scanner sc){
+        this.sc = sc;
+    }
+    public void run() {
+        System.out.println(("==명언 앱=="));
+
+        while (true){
+            System.out.print("명령)");
+            //trim() : 혹시 있을지 모를 좌우공백제거
+            String command = sc.nextLine().trim();
+
+            if (command.equals("종료")) {
+                break;
+            }
+        }
+
+    }
+}
